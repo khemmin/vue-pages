e989ba9c8731c55281b92cf1445a795331039212
khemmin <gokou_kem@hotmail.com>
2018-07-24 17:17:34 +0700

    import validate and router

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,13 +4,22 @@
 
       <form @submit.prevent="addSkill">
         <div class="ui icon input">
-        <input type="text" placeholder="Enter a skill you have.." v-model="skill">
+        <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated filpOutX">
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
         <i class="inverted circular search link icon"></i>
         </div>
       </form>
       
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key='index'>
+            {{data.skill}}
+            <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+          </li>
+
+        </transition-group>
       </ul>
 
     <p>These are the skills that you possess. </p>
@@ -32,8 +41,17 @@ export default {
   },
   methods: {
     addSkill(){
-      this.skills.push({skill: this.skill})
-      this.skill = '';
+      this.$validator.validateAll().then((result) => {
+          if (result) {
+            this.skills.push({skill: this.skill});
+            this.skill = '';
+          } else {
+            console.log('Not valid');
+          }
+        })
+    },
+    remove(id) {
+      this.skills.splice(id,1);
     }
   }
 }
@@ -41,6 +59,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
 .holder {
     background: #fff;
@@ -71,13 +92,40 @@ export default {
     box-shadow: 0px 0px 40px lightgray;
   }
 
-  /* input {
+  input {
     width: calc(100% - 40px);
     border: 0;
     padding: 20px;
     font-size: 1.3em;
     background-color: #323333;
     color: #687F7F;
-  } */
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+  animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
 
 </style>
